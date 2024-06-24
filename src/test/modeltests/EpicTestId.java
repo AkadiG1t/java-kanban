dc6bfcd5119bd1db4ce9f8062306f8c717f920ba
AkadiG1t <164893903+AkadiG1t@users.noreply.github.com>
2024-06-24 19:26:45 +0300

    Исправить ошибки в CheckStyle в EpicTestId

--- src/test/modeltests/EpicTestId.java
+++ src/test/modeltests/EpicTestId.java
@@ -11,8 +11,8 @@ import service.Managers;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 class EpicTestId {
-    Epic epic = new Epic("newEpic", "newDescription");
-    Task epic2 = new Epic("newEpic", "newDescription");
+    Epic epic = new Epic("newEpic1", "newDescription1");
+    Task epic2 = new Epic("newEpic2", "newDescription2");
 
     @Test
     public void epicEquals() {
@@ -22,38 +22,31 @@ class EpicTestId {
     }
 
     static class InMemoryTaskManagerTest {
-    
         InMemoryTaskManager inMemoryTaskManager = new InMemoryTaskManager(Managers.getDefaultHistory());
-        Task task = inMemoryTaskManager.createTask(new Task("newTask", "newDescription"));
-        Epic epic = inMemoryTaskManager.createEpic(new Epic("newEpic", "newDescription"));
-        Task subTask = inMemoryTaskManager.createSubTask(new SubTask("newSubTask", "newDescription"),
+        Task task = inMemoryTaskManager.createTask(new Task("newTask", "newDescription1"));
+        Epic epic = inMemoryTaskManager.createEpic(new Epic("newEpic", "newDescription2"));
+        Task subTask = inMemoryTaskManager.createSubTask(new SubTask("newSubTask", "newDescription3"),
                 epic);
-    
         @Test
         void createTask() {
             assertNotNull(task);
         }
-    
         @Test
         void getTask() {
             assertNotNull(inMemoryTaskManager.getTask(task.getId()));
         }
-    
         @Test
         void createEpic() {
             assertNotNull(epic);
         }
-    
         @Test
         void getEpic() {
             assertNotNull(inMemoryTaskManager.getEpic(epic.getId()));
         }
-    
         @Test
         void getSubtask() {
             assertNotNull(inMemoryTaskManager.getSubtask(subTask.getId()));
         }
-    
         @Test
         void createSubTask() {
             assertNotNull(subTask);
